ab98712d9057b9c91d64d1cca438b3c35a5313e5
Hardik <hardikholani27@gmail.com>
2026-07-27 13:05:40 +0530

    fix: explicit prisma import

--- Server/configs/prisma.js
+++ Server/configs/prisma.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from "../generated/prisma/index.js";
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
